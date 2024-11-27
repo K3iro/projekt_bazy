86e2d5c540fd8ca7dde04cbcdb0a537a72f4be91
Kuba Szadok <144441304+K3iro@users.noreply.github.com>
2024-11-27 14:57:00 +0100

    Add files via upload

---
+++ cone.php
@@ -0,0 +1,16 @@
+<?php
+$server="127.0.0.1";
+$user="root";
+$password="";
+$baza="klubpilkarski";
+$conn = mysqli_connect($server,$user,$password,$baza)
+or ("błąd połączenia".mysqli_connect_error())
+
+
+
+
+
+
+
+
+?>
